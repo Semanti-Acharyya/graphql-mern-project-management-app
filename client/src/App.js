3e7f73c5fe600492ddee76b0d2f3840fede0fff1
Semanti-Acharyya <sacharyya2000@gmail.com>
2025-05-20 20:32:38 +0530

    Added client data to the webpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import Header from "./components/Header";
+// using InMemoryCache so that the added data shows up right
+// away on the UI and we don't have to refresh the page
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Clients from "./components/Clients";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5001/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello world</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
     </>
   );
 }
